6d2fc553d2fa788ba5148467c6cb7325930f4bf8
Ben Lewis <benjf5+bitbucket@gmail.com>
2013-01-26 10:30:10 -0500

    dlman: So. It does not have an interface, and it downloads to the current directory. But it downloads, and it downloads well.

--- dlman.ml
+++ dlman.ml
@@ -38,25 +38,28 @@ let write_wrapper file_digest connection fp data_buffer data_line =
   write_data fp data_buffer data_line
     
 let dl_file file_location =
-  let location_list = Str.split (Str.regexp "/") file_location in
-  let file_name = List.hd (List.rev location_list) in
-  let file_p = open_out_bin file_name in
-  let file_digest = Digest.string file_location in
-  let incoming_data = Buffer.create 4096 (* using a simple buffer at the
-  moment. This will become configurable. *)
-  and connection = Curl.init () in
-  Curl.set_writefunction connection (write_wrapper file_digest connection file_p incoming_data);
-  Curl.set_followlocation connection true;
-  Curl.set_url connection file_location;
-  Curl.perform connection;
-  Curl.cleanup connection;
-  print_string ("Download complete: " ^ file_name);
-  close_out file_p
+  try 
+    let location_list = Str.split (Str.regexp "/") file_location in
+    let file_name = List.hd (List.rev location_list) in
+    let file_p = open_out_bin file_name in
+    let file_digest = Digest.string file_location in
+    let incoming_data = Buffer.create 4096 (* using a simple buffer at the
+                                              moment. This will become configurable. *)
+    and connection = Curl.init () in
+    Curl.set_writefunction connection (write_wrapper file_digest connection file_p incoming_data);
+    Curl.set_followlocation connection true;
+    Curl.set_url connection file_location;
+    Curl.perform connection;
+    Curl.cleanup connection;
+    print_string ("Download complete: " ^ file_name);
+    close_out file_p
+  with Curl.CurlException (_, _, s) -> print_string s
 
 (* Needs functions to produce a webpage, now that it downloads files. *)
 
 let add_download file_location =
   Thread.create dl_file file_location
+      
     (* This function will need to become slightly more complicated, and it will
   need a function around dl_file, since we'll be handling sending output to a
   webpage (if launched), etc. *)
@@ -86,6 +89,7 @@ let add_download file_location =
 let send_job file_location =
   try
     let dlman_add_sock = Unix.socket Unix.PF_UNIX Unix.SOCK_DGRAM 0 in
+    Unix.bind dlman_add_sock (Unix.ADDR_UNIX("/tmp/dlman_add_socket"));
     Unix.connect dlman_add_sock (Unix.ADDR_UNIX("/tmp/dlman_socket"));
     let msg_len = String.length file_location in
     let char_mask = (lnot ((lnot 0) lsl 8)) in
@@ -94,21 +98,22 @@ let send_job file_location =
     msg_len_str.[1] <- (char_of_int (char_mask land (msg_len lsr 8)));
     msg_len_str.[2] <- (char_of_int (char_mask land (msg_len lsr 16)));
     msg_len_str.[3] <- (char_of_int (char_mask land (msg_len lsr 24)));
-    let send_state = Unix.send dlman_add_sock msg_len_str 0 4 [] in
-    ignore (send_state = Unix.send dlman_add_sock file_location 0 msg_len [])
+    ignore (Unix.send dlman_add_sock msg_len_str 0 4 []);
+    ignore (Unix.send dlman_add_sock file_location 0 msg_len []);
+    Unix.close dlman_add_sock;
+    Sys.remove "/tmp/dlman_add_sock"
   with Unix.Unix_error (err, cmd, loc) ->
-    print_string "Failed to send job to daemon."
+    (print_string "Failed to send job to daemon.";
+     Sys.remove "/tmp/dlman_add_sock")
       
 let read_loop download_dir =
   try
     let dlman_sock = Unix.socket Unix.PF_UNIX Unix.SOCK_DGRAM 0 in
     Unix.bind dlman_sock (Unix.ADDR_UNIX("/tmp/dlman_socket"));
-    Unix.listen dlman_sock 5;
-    let (add_addr, add_sock) = Unix.accept dlman_sock in
       try
         while true do
           let msg_size_str = String.create 4 in
-          ignore (Unix.recv dlman_sock msg_size_str 0 4 []);
+          let received_chars = Unix.recv dlman_sock msg_size_str 0 4 [] in
           let msg_size = ((int_of_char msg_size_str.[0]) +
                              ((int_of_char msg_size_str.[1]) lsl 8) +
                              ((int_of_char msg_size_str.[2]) lsl 16) + 
@@ -120,19 +125,22 @@ let read_loop download_dir =
       with Unix.Unix_error (error, cmd, loc) ->
         print_string "Unable to receive request.\n"
   with Unix.Unix_error (err, cmd, loc) ->
-    print_string "Unable to launch daemon!\n"
+    print_string ("Unable to launch daemon! Generated error in " ^ cmd ^ "\n");
+    Printf.eprintf "Error code: %s\n" (Unix.error_message err);
+    Sys.remove "/tmp/dlman_socket"
 (* That loop will run forever, at least right now. I should create a method
    to determine when it should be cancelled. *)
     (* This does not remove the fifo currently. That needs to be resolved. *)
 
-let _ = ()
-(*  let dl_dir = "/Users/ben/Downloads" in (* This needs to become loaded from a
+let _ =
+  Curl.global_init Curl.CURLINIT_GLOBALALL;
+  let dl_dir = "/Users/ben/Downloads" in (* This needs to become loaded from a
   file. *)
   if (Array.length Sys.argv) > 2 then
     if (String.compare (Sys.argv.(1)) "--add") = 0 then
-      if (Sys.file_exists (dl_dir ^ "/.dlman.daemon.pid")) then
-        for i=1 to ((Array.length Sys.argv) - 1) do
-          send_job (Sys.argv.(i) ^ "\n")
+      if (Sys.file_exists ("/tmp/dlman_socket")) then
+        for i=2 to ((Array.length Sys.argv) - 1) do
+          send_job (Sys.argv.(i))
         done
       else
         print_string "There does not appear to be a dlman daemon running!\n"
@@ -140,15 +148,12 @@ let _ = ()
       print_string "Incorrect invocation of dlman.\n"
   else
     if (Array.length Sys.argv) = 2 then
-      let new_thread = add_download Sys.argv.(1) in
-      print_string "Waiting on download . . .\n";
-      Thread.join new_thread;
-      print_string "Finished waiting.\n"
-    else if (String.compare (Sys.argv.(1)) "--start-daemon") = 0 then
-      let daemon_lock = acquire_lock dl_dir in
-      if daemon_lock = 0 then
+      if (String.compare Sys.argv.(1) "--start-daemon" = 0) then
         read_loop dl_dir
       else
-        print_string "Unable to start dlman daemon.\n"
+        let new_thread = add_download Sys.argv.(1) in
+        print_string "Waiting on download . . .\n";
+        Thread.join new_thread;
+        print_string "Finished waiting.\n"
     else
-      print_string "Incorrect invocation of dlman.\n" *)
+      print_string "Incorrect invocation of dlman.\n"
