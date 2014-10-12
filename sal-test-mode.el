69187b4964eed11b16a509fe102de3ccf37eeb14
Ben Lewis <benjf5+bitbucket@gmail.com>
2014-10-11 20:48:46 -0700

    Completed the regex for nonparametric function parameters, works for basic cases.

--- sal-test-mode.el
+++ sal-test-mode.el
@@ -1,5 +1,7 @@
 ;; Copyright (C) 2014 Ben Lewis <benjf5@gmail.com>
 
+(require 'cc-mode)
+
 ;; define SAL faces
 (defface sal-function-param-face
   '((t (:inherit font-lock-builtin-face))) ;; might not want builtin-face
@@ -7,15 +9,16 @@
 (defvar sal-function-param-face 'sal-function-param-face)
 ;; Additional faces for other parameter types should be added later.
 
-(defconst function-annotation-keywords
+(defconst nonparametric-function-annotation-keywords
   (list
-   '("\\<_In_\\>" 0 sal-function-param-face))
+   '("\\<_\\(In\\(out\\)?\\(_opt\\)?\\(_z\\)?\\|Out\\(ptr\\(_opt\\)?\\(_result\\(\\(\\(_maybenull\\|_nullonfailure\\)\\(_z\\)?\\)\\|_z\\)\\)?\\|\\(_opt\\)?\\|ref\\(_result\\(_maybenull\\|_nullonfailure\\)\\)\\)?\\|Ret\\(_null\\|_notnull\\|_z\\|_maybenull\\(_z\\)?\\)\\)_\\>" 0 sal-function-param-face))
+  ;; Capturing how SAL is represented in a regex is hard.
   "Keywords for annotating function parameters.")
 
 (defun sal-mode-add-font-locks ()
   (font-lock-add-keywords
    nil
-   function-annotation-keywords t))
+   nonparametric-function-annotation-keywords t))
 
 (define-minor-mode sal-mode
   "Support Microsoft SAL2 in C++-mode."
